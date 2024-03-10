3c2296ffc2dc750af424ba64071e1031e04944d9
jpdieter <justindieter@gmail.com>
2024-03-09 20:30:54 -0800

    fixed an issue with module.exports

--- models/genre.js
+++ models/genre.js
@@ -1,17 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const GenreSchema = new Schema({
-    name: {type: String, required: true, minLength: 3, maxLength: 100},
+  name: { type: String, required: true, minLength: 3, maxLength: 100 },
+});
 
-})
-
-// Virtual for genre's URL
+// Virtual for this genre instance URL.
 GenreSchema.virtual("url").get(function () {
-    // We don't use an arrow function as we'll need the this object
-    return `/catalog/genre/${this._id}`;
-  });
+  return "/catalog/genre/" + this._id;
+});
 
-  //Export model
- module.exports = mongoose.model("Genre", "GenreSchema")
+// Export model.
+module.exports = mongoose.model("Genre", GenreSchema);
