b82f37853bdbe2d9c603985ada7fee4f62e1ad04
jpdieter <justindieter@gmail.com>
2024-03-13 21:46:40 -0700

    added lifespan function to reformat DateTime

--- models/author.js
+++ models/author.js
@@ -28,6 +28,22 @@ AuthorSchema.virtual("url").get(function () {
   return `/catalog/author/${this._id}`;
 });
 
+AuthorSchema.virtual("lifespan").get(function () {
+    let lifetime_string = "";
+    if (this.date_of_birth) {
+      lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+        DateTime.DATE_MED
+      );
+    }
+    lifetime_string += " - ";
+    if (this.date_of_death) {
+      lifetime_string += DateTime.fromJSDate(this.date_of_death).toLocaleString(
+        DateTime.DATE_MED
+      );
+    }
+    return lifetime_string;
+  });
+
 AuthorSchema.virtual("date_of_birth_yyyy_mm_dd").get(function () {
     return DateTime.fromJSDate(this.date_of_birth).toISODate(); // format 'YYYY-MM-DD'
   });
@@ -35,6 +51,8 @@ AuthorSchema.virtual("date_of_birth_yyyy_mm_dd").get(function () {
   AuthorSchema.virtual("date_of_death_yyyy_mm_dd").get(function () {
     return DateTime.fromJSDate(this.date_of_death).toISODate(); // format 'YYYY-MM-DD'
   });
+
+  
 
 // Export model
 module.exports = mongoose.model("Author", AuthorSchema);
