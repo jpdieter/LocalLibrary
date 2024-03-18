784c123f6163c2325b42a048659cf3fa8619156b
jpdieter <justindieter@gmail.com>
2024-03-18 16:21:50 -0700

    installed and set up rate limiter

--- app.js
+++ app.js
@@ -14,8 +14,16 @@ const catalogRouter = require("./routes/catalog"); //Import routes for "catalog"
 
 const app = express();
 
-// Set up mongoose connection
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
 
+// Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 const mongoDB = process.env.DATABASE_URL;
