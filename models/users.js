05cb2e828dad30730224b8d49ec040da1f4f6203
jpdieter <justindieter@gmail.com>
2024-03-24 22:48:03 -0700

    changed password to hashed_password in UserSchema to conform to Passport LocalStrategy

--- models/users.js
+++ models/users.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
+// const passportLocalMongoose = require('passport-local-mongoose');
 
-const userSchema = new Schema({
+const UserSchema = new Schema({
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    hashed_password: { type: String, required: true },
+    salt: { type: String, required: true }
   });
 
-UserSchema.plugin(passportLocalMongoose)
+// UserSchema.plugin(passportLocalMongoose)
 
 module.exports = mongoose.model('User', UserSchema)
