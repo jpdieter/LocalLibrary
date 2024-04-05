414380198f608c4771528cac3df98e374250ce4b
jpdieter <justindieter@gmail.com>
2024-04-04 18:28:41 -0700

    re-enabled rate limiter

--- app.js
+++ app.js
@@ -21,15 +21,15 @@ const collectionRouter = require('./routes/collection');
 const searchRouter = require('./routes/search');
 const app = express();
 
-// // Set up rate limiter: maximum of twenty requests per minute
-// const RateLimit = require("express-rate-limit");
-// const limiter = RateLimit({
-//   windowMs: 1 * 60 * 1000, // 1 minute
-//   max: 20,
-//   validate: {xForwardedForHeader: false},
-// });
-// // Apply rate limiter to all requests
-// app.use(limiter);
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+  validate: {xForwardedForHeader: false},
+});
+// Apply rate limiter to all requests
+app.use(limiter);
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
