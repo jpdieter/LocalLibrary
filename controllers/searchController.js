393a958876c7534f2399e53e9309ce32ba7056eb
jpdieter <justindieter@gmail.com>
2024-04-09 23:45:44 -0700

    created handleSearch logic to search for books by title

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,3 +1,4 @@
+const book = require('../models/book');
 const search = require('../models/search');
 
 // search.js
@@ -8,13 +9,29 @@ exports.showSearchPage = function(req, res) {
     res.render('search', {title: 'Search'});
   };
   
-// Handle search form submission
-exports.handleSearch = function(req, res) {
-    const searchTerm = req.body.searchTerm;
+  const Book = require("../models/book");
 
-    // Perform search logic here...
-    // Example: Query the database for search results
-
-    // For demonstration purposes, let's just send back the search term
-    res.send('Search results for: ' + searchTerm);
-};
+  // Handle search form submission
+  exports.handleSearch = function(req, res) {
+    const searchTerm = req.body.q;
+  
+    // Perform search logic
+    Book.find({
+      $or: [
+        { title: { $regex: new RegExp(searchTerm, 'i') } }, // Search in title
+        { isbn: { $regex: new RegExp(searchTerm, 'i') } },   // Search in ISBN
+      ]
+    })
+    .populate('author') // Assuming you want to populate author information
+    .then(results => {
+      res.render('searchResults', { title: 'Search Results', results });
+    })
+    .catch(err => {
+      console.error('Error occurred during search:', err);
+      res.status(500).send('Internal Server Error');
+    });
+  };
+  
+    
+    // // For demonstration purposes, let's just send back the search term
+    // res.send('Search results for: ' + searchTerm);
