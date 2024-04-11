bfee4b3017096b11ad047368b6874021d833c46a
jpdieter <justindieter@gmail.com>
2024-04-10 18:55:48 -0700

    added logic to search by author and genre

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,5 +1,7 @@
-const book = require('../models/book');
-const search = require('../models/search');
+const Book = require("../models/book"); // Import the Book model
+const SearchLog = require('../models/search');
+const Genre = require('../models/genre');
+const Author = require('../models/author');
 
 // search.js
 
@@ -8,30 +10,41 @@ exports.showSearchPage = function(req, res) {
     // Handle search logic here
     res.render('search', {title: 'Search'});
   };
-  
-  const Book = require("../models/book");
 
   // Handle search form submission
   exports.handleSearch = function(req, res) {
     const searchTerm = req.body.q;
   
-    // Perform search logic
+   // Perform search logic
+  Promise.all([
+    // Search for books by title or ISBN
     Book.find({
       $or: [
         { title: { $regex: new RegExp(searchTerm, 'i') } }, // Search in title
-        { isbn: { $regex: new RegExp(searchTerm, 'i') } },   // Search in ISBN
+        { isbn: { $regex: new RegExp(searchTerm, 'i') } }   // Search in ISBN
       ]
-    })
-    .populate('author') // Assuming you want to populate author information
-    .then(results => {
-      res.render('searchResults', { title: 'Search Results', results });
-    })
-    .catch(err => {
-      console.error('Error occurred during search:', err);
-      res.status(500).send('Internal Server Error');
+    }).populate('author'), // Populate author information for each book
+    // Search for authors by name
+    Author.find({
+      $or: [
+        { first_name: { $regex: new RegExp(searchTerm, 'i') } }, // Search in first name
+        { family_name: { $regex: new RegExp(searchTerm, 'i') } }  // Search in last name
+      ]
+    }),
+    // Search for genres by name
+    Genre.find({ name: { $regex: new RegExp(searchTerm, 'i') } })
+  ])
+  .then(([bookResults, authorResults, genreResults]) => {
+    res.render('searchResults', {
+      title: 'Search Results',
+      searchTerm,
+      bookResults,
+      authorResults,
+      genreResults
     });
-  };
-  
-    
-    // // For demonstration purposes, let's just send back the search term
-    // res.send('Search results for: ' + searchTerm);
+  })
+  .catch(err => {
+    console.error('Error occurred during search:', err);
+    res.status(500).send('Internal Server Error');
+  });
+};
