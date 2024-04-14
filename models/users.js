38bcd156a817b1f09970f8a7bd4b25f1fb1a9185
jpdieter <justindieter@gmail.com>
2024-04-13 18:21:29 -0700

    created changePassword method that validates current password and generates a new one

--- models/users.js
+++ models/users.js
@@ -1,11 +1,47 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-// const passportLocalMongoose = require('passport-local-mongoose');
+const crypto = require('crypto');
 
 const UserSchema = new Schema({
     username: { type: String, required: true, unique: true },
     hashed_password: { type: String, required: true },
     salt: { type: String, required: true }
-  });
+});
 
-module.exports = mongoose.model('User', UserSchema)
+// Define the changePassword method
+UserSchema.methods.changePassword = function(oldPassword, newPassword) {
+  return new Promise((resolve, reject) => {
+    const user = this;
+
+    // First, validate the current password
+    const oldPasswordBuffer = Buffer.from(oldPassword, 'utf-8');
+    crypto.pbkdf2(oldPasswordBuffer, user.salt, 310000, 32, 'sha256', (err, derivedKey) => {
+      if (err) {
+        reject(err);
+      } else {
+        const currentHash = derivedKey.toString('base64');
+        if (currentHash !== user.hashed_password) {
+          reject(new Error('Current password is incorrect'));
+          return;
+        }
+
+        // Generate a new salt and hash the new password
+        user.salt = crypto.randomBytes(16).toString('base64');
+        const newPasswordBuffer = Buffer.from(newPassword, 'utf-8');
+        crypto.pbkdf2(newPasswordBuffer, user.salt, 310000, 32, 'sha256', (err, derivedKey) => {
+          if (err) {
+            reject(err);
+          } else {
+            user.hashed_password = derivedKey.toString('base64');
+            user.save()
+              .then(() => resolve())
+              .catch(error => reject(error));
+          }
+        });
+      }
+    });
+  });
+};
+
+
+module.exports = mongoose.model('User', UserSchema);
