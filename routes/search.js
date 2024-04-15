6d34d12f07635dbdeab72b946d3f91984f848c1d
jpdieter <justindieter@gmail.com>
2024-04-15 10:05:41 -0700

    redirected /search to /collection

--- routes/search.js
+++ routes/search.js
@@ -4,8 +4,14 @@ const express = require('express');
 const router = express.Router();
 const searchController = require('../controllers/searchController');
 
+// // GET search page
+// router.get('/', searchController.showSearchPage);
+
 // GET search page
-router.get('/', searchController.showSearchPage);
+router.get('/', (req, res) => {
+    res.redirect('/collection');
+});
+
 
 // POST search form submission
 router.post('/submit', searchController.handleSearch); 
