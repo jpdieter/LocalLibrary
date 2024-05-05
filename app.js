d9fda9d0dc9cdf6b93fe6f76c911ef2620f37ea7
jpdieter <justindieter@gmail.com>
2024-05-05 12:01:43 -0700

    updated rate limiter max

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const app = express();
 const RateLimit = require("express-rate-limit");
 const limiter = RateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 30,
+  max: 35,
   validate: {xForwardedForHeader: false},
 });
 // Apply rate limiter to all requests
