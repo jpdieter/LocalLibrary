aa358f6bbbfe7da9da9f8d25b1d594d68c58a93c
jpdieter <justindieter@gmail.com>
2026-04-27 17:58:19 -0700

    Add end-to-end Playwright test for book details page navigation and validation

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -15,3 +15,18 @@ test('User can navigate to book catalog from landing page', async ({ page }) => 
   // Assert content
   await expect(page.getByRole('heading', { name: 'Book List' })).toBeVisible();
 });
+
+test('User can navigate to book details page', async ({ page }) => {
+
+  await page.goto('https://locallibrary-5sbd.onrender.com/collection');
+
+  await page.getByRole('link', { name: 'Total Books:' }).click();
+
+  await page.getByRole('link', { name: '1984' }).click();
+
+  // Assert URL
+  await expect(page).toHaveURL(/\/catalog\/book\//);
+
+  // Assert content
+  await expect(page.getByRole('heading', { name: /1984/ })).toBeVisible();
+});
